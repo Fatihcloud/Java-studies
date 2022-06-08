ac7a93e964137334b54ae42e7c131a2b567a5eb8
Fatih BULUT <bulut.fth.66@outlook.com>
2022-06-08 17:19:14 +0300

    Add files via upload

---
+++ Java-101/src/CinZodyagı.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class CinZodyagı {
+    public static void main(String[] args) {
+        int sayı1;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınız : ");
+        sayı1 = inp.nextInt();
+
+        int toplam = sayı1%12;
+
+        switch (toplam){
+            case 0:
+                System.out.print("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.print("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.print("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.print("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.print("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.print("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.print("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.print("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.print("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.print("Çin Zodyağı Burcunuz : Koyun");
+                break;
+            default:
+                System.out.print("Bir şeyler yanlış gitti");
+                break;
+        }
+    }
+}
