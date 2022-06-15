0e3745b1fa240de23dfe1f76a47f915d9d5b33bd
Fatih BULUT <bulut.fth.66@outlook.com>
2022-06-15 19:24:57 +0300

    Add files via upload

---
+++ Java-101/src/TersUcgen.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.print("Sayi giriniz : ");
+        n=scanner.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=2*n-1;k>=(2*i+1);k--)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
