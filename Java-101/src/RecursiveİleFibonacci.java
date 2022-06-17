e0febc0dfe0495dc8df0c0d5807bc381b5a79aff
Fatih BULUT <bulut.fth.66@outlook.com>
2022-06-17 15:29:12 +0300

    Add files via upload

---
+++ Java-101/src/RecursiveİleFibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RecursiveİleFibonacci {
+    static int fib(int n){
+        if (n == 1 || n == 2){
+            return 1;
+        }
+        return fib(n - 1) + fib (n-2);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        int a = input.nextInt();
+        System.out.println(fib(a));
+    }
+}
